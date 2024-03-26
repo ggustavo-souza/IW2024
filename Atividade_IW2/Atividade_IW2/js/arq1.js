fc3ade0d4a39fe1ffd4b1fabae0f8db125f73060
ggustavo-souza <160797699+ggustavo-souza@users.noreply.github.com>
2024-03-25 22:20:34 -0300

    Add files via upload

---
+++ Atividade_IW2/Atividade_IW2/js/arq1.js
@@ -0,0 +1,27 @@
+//Atividade 1
+alert("Oie migo qual aquela música que a gente não para de cantar mesmo?")
+
+function msg() {
+    //Atividade 2 
+    alert("Oi Emerson.");
+}
+
+function limonations() {
+    //Atividade 3.1
+    document.getElementById("demo").innerHTML = "Limonations";
+}
+function megalimonation() {
+    document.getElementById("demo").innerHTML = "Meu suco favorito é...";
+}
+
+function imag(x) {
+    x.style.height="30%";
+    x.style.width="30%";
+}
+function imaag(x) {
+    x.style.height="25%";
+    x.style.width="25%";
+}
+
+
+
